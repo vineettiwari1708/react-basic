d16ecb4727527105470a7d6ad38d677569c871d2
unknown <vineettiwari1708@gmail.com>
2024-04-12 00:00:53 +0530

    basic react

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-const heading = React.createElement(
-  "div",
-  { id: "heading" },
-  React.createElement("div", { id: "heading" }, [
-    React.createElement("h1", { id: "heading1" }, "Hello React"),
-    React.createElement("h2", { id: "heading2" }, "Hello React"),
-  ])
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "Hello React1"),
+    React.createElement("h2", {}, "Hello React2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hello React1"),
+    React.createElement("h2", {}, "Hello React2"),
+  ]),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
